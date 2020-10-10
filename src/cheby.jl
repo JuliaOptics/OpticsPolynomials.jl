3bb2a2bfcb1defaaecc2e394ab40ec496054893b
Brandon <brandondube@gmail.com>
2020-10-10 15:50:21 -0700

    cheby: fix naming bug -- cheby2 redef of cheby1

--- src/cheby.jl
+++ src/cheby.jl
@@ -20,6 +20,6 @@ Compute the Chebyshev polynomial of the second kind of order n at point x.
 This family of Chebyshev polynomials are a special case of the Jacobi polynomials
 with α, β = 1/2.
 """
-function cheby1(n, x)
+function cheby2(n, x)
     return Jacobi.jacobi(n, 0.5, 0.5, x)
 end
