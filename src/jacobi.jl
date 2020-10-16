53d35119f1aee0d8e4c75d7a381dfbea7b38dd18
Brandon <brandondube@gmail.com>
2020-10-15 21:40:23 -0700

    jacobi: restore optimized routine (went missing??)
    also includes bugfixes from broadcasting / dot operators

--- src/jacobi.jl
+++ src/jacobi.jl
@@ -10,9 +10,14 @@ function jacobi_weight(α, β, x)
 end
 
 """
-    abc(n, α, β, x)
+	calcac/b(n, α, β, x)
 
-Compute the three coefficients in the recurrence relation for the Jacobi polynomial.
+Two functions calcac and calcb compute the three coefficients in the recurrence
+relation for the Jacobi polynomial.
+
+It is broken into two functions to separate a and c, which do not depend on x
+and can be calculated once for a vector of x, from b which does depend on x and
+must be calculated for each element.
 
 n is the order of the polynomial.
 
@@ -20,15 +25,19 @@ n is the order of the polynomial.
 
 x is the argument or point at which the polynomial will be evaluated.
 """
-function abc(n, α, β, x)
+function calcac(n, α, β)
+    a = (2n) * (n + α + β) * (2n + α + β - 2)
+    c = 2 * (n + α - 1) * (n + β - 1) * (2n + α + β)
+    return a, c
+end
+
+@inline function calcb(n, α, β, x)
 	# the parens emphasize the terms, not PEMDAS
-	a = (2n) * (n + α + β) * (2n + α + β - 2)
 	b1 = (2n + α + β -1)
 	b2 = (2n + α + β)
 	b2 = b2 * (b2 - 2)
 	b = b1 * (b2 * x + α^2 - β^2)
-	c = 2 * (n + α - 1) * (n + β - 1) * (2n + α + β)
-	return a, b, c
+	return b
 end
 
 """
@@ -42,28 +51,31 @@ is also linear w.r.t. the size of argument x.  x should be passed as an array,
 and jacobi should not be called with dot syntax, for best performance.
 """
 function jacobi(n, α, β, x)
-	if n == 0
-		return 1.
-	elseif n == 1
-		return (α + 1) + (α + β + 2) * ((x-1)/2)
+    if n == 0
+        return ones(size(x))
+    elseif n == 1
+        return (α + 1) .+ (α + β + 2) .* ((x.-1)./2)
+    end
+	# Pnm2 = 1, skip because it is waste calculation
+	# do scalar division on a to avoid vector division
+	# the cod ebelow, therefore, visually does not
+	# match the math, though it is correct
+    Pnm1 = (α + 1) .+ (α + β + 2) .* ((x.-1)./2)
+	a, c = calcac(2, α, β)
+	inva = 1 / a
+    Pn = ((calcb.(2, α, β, x) .* Pnm1) .- c) .* inva
+    if n == 2
+        return Pn
+    end
+    Pnm2 = similar(Pnm1)
+    for i = 3:n
+        Pnm2, Pnm1 = Pnm1, Pn
+        a, c = calcac(n, α, β)
+        inva = 1 / a
+        # @avx unroll=4 for i in eachindex(Pn)
+            # Pn[i] = (calcb(n, α, β, x[i]) * Pnm1[i] - c * Pnm2[i]) * inva
+		# end
+        Pn = ((calcb.(n, α, β, x) .* Pnm1) .- (c .* Pnm2)) ./ a
 	end
-	# this uses a loop to avoid recursion
-	# we init P of n-2, n-1, and n=2.
-	# the latter is the first recursive term.
-	# then loop from 3 up to n, updating
-	# Pnm2, Pnm1, a, b, c, Pn
-	Pnm2 = 1.  # == jacobi(1, ...)
-	Pnm1 = (α + 1) + (α + β + 2) * ((x-1)/2)  # == jacobi(2, ...)
-	a, b, c = abc(2, α, β, x)
-	Pn = ((b * Pnm1) - (c * Pnm2)) / a
-	if n == 2
-		return Pn
-	end
-	for i = 3:n
-		Pnm2 = Pnm1
-		Pnm1 = Pn
-		a, b, c = abc(n, α, β, x)
-		Pn = ((b * Pnm1) - (c * Pnm2)) / a
-	end
-	return Pn
+    return Pn
 end
