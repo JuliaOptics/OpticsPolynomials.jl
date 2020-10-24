75d728928a554701b5d8e8f31066356bab46ad28
Brandon Dube <brandondube@gmail.com>
2020-10-24 10:46:50 -0700

    + travis

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,4 @@
 using Documenter, OpticsPolynomials
 
 makedocs(modules = [OpticsPolynomials], doctest = false, sitename = "OpticsPolynomials")
-# deploydocs(repo = "github.com/JuliaOptics/GridCreation.jl.git")
+deploydocs(repo = "github.com/JuliaOptics/GridCreation.jl.git")
