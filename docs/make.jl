64951faba4b9315dbc324ecf969e6dd4ad5b6124
Brandon Dube <brandondube@gmail.com>
2020-10-24 11:27:46 -0700

    docs: devbranch => main

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,4 @@
 using Documenter, OpticsPolynomials
 
 makedocs(modules = [OpticsPolynomials], doctest = false, sitename = "OpticsPolynomials")
-deploydocs(repo = "github.com/JuliaOptics/OpticsPolynomials.jl.git")
+deploydocs(repo = "github.com/JuliaOptics/OpticsPolynomials.jl.git", devbranch="main")
